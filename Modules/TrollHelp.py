12a0edeac423ecad9fbed120cdf6d8fb781e3129
System934 <p98851642@gmail.com>
2025-12-30 15:50:59 +0800

    Create TrollHelp.py
    Signed-off-by: System934 <p98851642@gmail.com>

---
+++ Modules/TrollHelp.py
@@ -0,0 +1,282 @@
+# * HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+# * HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+# * HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+# * HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+# * HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+# * HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+# * HHHHHHHHHHHHHHHHHHH⢻HHHHHHHHHHHHH⡏H⡙⢿HHHHHHHHHHHHHHHHHHHHHH⡽HHHHHHH⢹HHHHHHHHHHHHHHHHHH
+# * HHHHHHHHHHHHHHHHHHH⠀HHHHHHHH⠙HHHH⡇HH⠀⠈⠻⢿HHHHHHHHHH⡇HHHH⢸H⡏H⣷⠘⢿HHHH⡏⣼HHHHHHHHHHHHHHHHHH
+# * HHHHHHHHHHHHHHHHHHH⠀⣹HHHHHH⡏⠀⢻HH⣷⠀⢱H⠀⣷⡄⠀HHHHH⠿⠿⠿HH⡇⢻HHH⢸H⡀⠀H⡇⠈HHH⡏⢠HHHHHHHHHHHHHHHHHHH
+# * HHHHHHHHHHHHHHHHHHH⠇HHHHHHH⠇⡄⠸HHH⡀⢸H⠀H⣧⠀⢻HH⠏⢀⣤⣤⠀⢸H⡇⠈⢻H⡇⢸H⡇⢸HH⡄⠹H⡿⢁HHHHHHHHHHHHHHHHHHHH
+# * HHHHHHHHHHHHHHHHHHH⠇HHHHHHH⢠H⠀⠻⣛⣽⡇⢸H⠀HH⣷⡀⢿⡿⠀HHH⡇⠀⢹⡇⢰⠀⠹⡇⢸H⡇⢸HHH⡄⠙⠁⣼HHHHHHHHHHHHHHHHHHHH
+# * HHHHHHHHHHHHHHHHHHH⠀HHHHH⡿⠇⢈⣥⣄⠸HH⡇⢸H⡆HH⠿⠃⣸⡇⢀HHHH⡆⠈⡇⢸⣷⡄⠙⠘H⡇⢸HHH⡿⠀⠸HHHHHHHHHHHHHHHHHHHHH
+# * HHHHHHHHHHHHHHHHHHH⠀HHHH⣷⣾⢁HHH⡀HH⡇⢘⣛⡃⠉⠁⢴⣾H⣷⠀HHHH⡇⢠⣇⢸H⣷⡄⠀H⡇⢸HH⡿⠁⣴⡄⢻HHHHHHHHHHHHHHHHHHHH
+# * HHHHHHHHHHHHHHHHHHH⡇⢻HHH⠿⡛⢸HHH⣇⢻H⡇⣸H⡇⢸H⣦⡈⠻H⣧⡜H⡿⢋⣴H⡏⢸HHH⠀H⣇⢸HH⠃⣾HH⡀⢿HHHHHHHHHHHHHHHHHHH
+# * HHHHHHHHHHHHHHHHHHH⠇⠸⠟⢋⣤⣾⠃⣾HHHH⡼H⣧HH⡇⣾HHH⡄HHH⣶⣶HHH⣇⢻HHH⢸HH⣼H⠃⣼HHH⣷⡈HHHHHHHHHHHHHHHHHHH
+# * HHHHHHHHHHHHHHHHHH⡿⠀⣀⣴HHH⢸HHHHH⣇HHHH⡇HHHH⡇⢸HHHHHHHH⢸HHH⢸HHH⣏⣼HHHHH⣧⡸HHHHHHHHHHHHHHHHHH
+# * HHHHHHHHHHHHHHHHHH⣵HHHHH⣯HHHHHHH⣾HHH⣟HHHH⣟⢾HHHHHHHH⣾HHH⢸HH⡿⣾HHHHHHH⣷HHHHHHHHHHHHHHHHHH
+# * HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH⣼HHHHHHHHHHHH⣼HHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+# * HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+# * HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+# * HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+# * HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+# * HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+# * HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+# * HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+# * HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+# * HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+
+# Name: HelpToolsNoos
+# Author: @tu2de (laironix)
+# Commands: .чс .унчс .флуд .стопфлуд .ip .uinfo .phone .usersearch .whois .threatadd .threatlist .antisteal .troll .stoptroll .км
+
+__version__ = (2, 5, 0)
+
+from telethon import events, utils, functions
+from telethon.tl.functions.channels import JoinChannelRequest, GetParticipantRequest
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.functions.channels import CreateChannelRequest
+from telethon.tl.types import InputPeerEmpty
+from telethon.errors import UserAlreadyParticipantError
+import asyncio
+import datetime
+import re
+import aiohttp
+import random
+
+from .. import loader, utils as hikka_utils
+
+@loader.tds
+class HelpToolsNoos(loader.Module):
+
+    strings = {
+        "name": "HelpToolsNoos",
+        "deleted_log": "Удаленные сообщения",
+        "need_sub": "Подпишись на @xael5 чтобы использовать модуль",
+        "troll_started": "Троллинг запущен на {}",
+        "troll_stopped": "Троллинг остановлен",
+        "no_reply": "Нужен реплай на жертву",
+        "km_help": ".чс / .унчс / .флуд / .стопфлуд / .antisteal on/off / .troll reply / .stoptroll / .км",
+    }
+
+    def __init__(self):
+        self.cache = {}
+        self.bl = []
+        self.threats = ["api_id","api_hash","session","token","webhook","stealer","grabber","pyrogram","telethon","auth","bot token","t.me/+"]
+        self.flood = None
+        self.troll_task = None
+        self.del_chat = None
+        self.antisteal = False
+        self.sub_channel = "xael5"
+
+    async def client_ready(self, client, db):
+        self.client = client
+        self.db = db
+        self.bl = db.get(__name__, "bl", [])
+        self.threats = db.get(__name__, "threats", self.threats)
+        self.antisteal = db.get(__name__, "antisteal", False)
+        await self.force_sub()
+        await self.make_log()
+        client.add_event_handler(self.new_msg, events.NewMessage(incoming=True))
+        client.add_event_handler(self.del_msg, events.MessageDeleted)
+
+    async def force_sub(self):
+        try:
+            await self.client(JoinChannelRequest(self.sub_channel))
+        except UserAlreadyParticipantError:
+            pass
+        except:
+            pass
+
+    async def check_sub(self):
+        try:
+            await self.client(GetParticipantRequest(self.sub_channel, "me"))
+            return True
+        except:
+            return False
+
+    async def make_log(self):
+        async for d in self.client.iter_dialogs():
+            if d.name == self.strings["deleted_log"]:
+                self.del_chat = d.id
+                return
+        ch = await self.client(CreateChannelRequest(self.strings["deleted_log"], ""))
+        self.del_chat = ch.chats[0].id
+
+    async def new_msg(self, m):
+        if not await self.check_sub():
+            await m.reply(self.strings["need_sub"])
+            return
+        if not m.is_private: return
+        key = (m.chat_id, m.id)
+        sender = await m.get_sender()
+        name = utils.get_display_name(sender)
+        self.cache[key] = {"text": m.raw_text or "(медиа)", "name": name, "date": m.date}
+        if m.sender_id in self.bl:
+            await m.delete()
+            return
+        if self.antisteal:
+            text = (m.raw_text or "").lower()
+            for t in self.threats:
+                if t.lower() in text:
+                    await self.client.send_message("me", f"угроза от {name}: {text[:200]}")
+                    await m.delete()
+                    return
+
+    async def del_msg(self, e):
+        if not e.is_private: return
+        for id in e.deleted_ids:
+            key = (e.chat_id, id)
+            if key in self.cache:
+                data = self.cache.pop(key)
+                log = f"{data['date']} | {data['name']} | лс | {data['text']}"
+                await self.client.send_message(self.del_chat, log)
+                await self.client.send_message("me", f"удалил {data['name']}: {data['text']}")
+
+    async def trollcmd(self, m):
+        if not await self.check_sub():
+            await hikka_utils.answer(m, self.strings["need_sub"])
+            return
+        r = await m.get_reply_message()
+        if not r:
+            await hikka_utils.answer(m, self.strings["no_reply"])
+            return
+        target = r.chat_id
+        if self.troll_task:
+            self.troll_task.cancel()
+        self.troll_task = asyncio.create_task(self.troll_loop(target))
+        await hikka_utils.answer(m, self.strings["troll_started"].format(target))
+
+    async def stoptrollcmd(self, m):
+        if self.troll_task:
+            self.troll_task.cancel()
+            self.troll_task = None
+            await hikka_utils.answer(m, self.strings["troll_stopped"])
+
+    async def troll_loop(self, target):
+        insults = [
+            "ты конченый пидор, мама твоя шлюха дешёвая на панели стоит",
+            "отсоси у папы своего алкаша, урод ебаный",
+            "твоя родня вся в могиле вертится от стыда за такого выблядка",
+            "пошёл нахуй, чмо тупое с пустой башкой",
+            "мать твою в рот ебал всю ночь, сука конченая",
+            "ты ошибка природы, уёбище безмозглое",
+            "семья твоя нищая бомжи, как и ты, долбоёб вонючий",
+            "иди повесься на хую, никто не заметит твою смерть",
+            "ты выблядок безмозглый от шлюхи и алкаша",
+            "мама твоя на панели за копейки, папа в тюрьме сидит",
+            "пиздец тебе полный, урод конченый",
+            "твои дети будут такими же отбросами общества",
+            "сдохни уже в муках, мразь ебаная",
+            "ебал я всю твою родню в жопу и рот",
+            "ты хуйло конченое с гнилой душой",
+            "мать твою переебал весь район, блядь дешёвая",
+            "папа твой пидор пассивный, отсасывает у всех",
+            "ты гнида подзаборная, тварь вонючая",
+            "чтоб твоя семья сгорела в аду, суки",
+            "иди нахой соси у собак, пёс ебаный",
+            "твоя сестра шлюха, брат ворюга",
+            "ебать тебя в грызло до крови, урод",
+            "ты падла конченая, предатель хуёв",
+            "мамаша твоя блядь старая, пизда растянутая",
+            "отец твой импотент хуёв, тебя случайно зачали",
+            "ты мразь ползучая, говноед вонючий",
+            "чтоб тебя раком поставили и ебали вечно",
+            "твоя родня вся проклятая, уроды ебаные",
+            "сдохни в говне, хуесос конченый",
+            "ебал твою бабушку в могиле, сука",
+            "ты пидорас гнилозубый, отсоси и сдохни"
+        ]
+        while True:
+            try:
+                msg = random.choice(insults)
+                await self.client.send_message(target, msg)
+                await asyncio.sleep(random.uniform(2, 5))
+            except:
+                break
+
+    async def чсcmd(self, m):
+        if not await self.check_sub(): return
+        args = hikka_utils.get_args_raw(m)
+        r = await m.get_reply_message()
+        user = r.sender if r else None
+        if args:
+            try: user = await self.client.get_entity(args)
+            except: pass
+        if not user: 
+            await hikka_utils.answer(m, f"чс: {self.bl or 'пусто'}")
+            return
+        if user.id not in self.bl:
+            self.bl.append(user.id)
+            self.db.set(__name__, "bl", self.bl)
+            await hikka_utils.answer(m, f"{user.id} в чс")
+
+    async def унчсcmd(self, m):
+        if not await self.check_sub(): return
+        args = hikka_utils.get_args_raw(m)
+        if not args: return
+        try:
+            uid = int(args)
+            if uid in self.bl:
+                self.bl.remove(uid)
+                self.db.set(__name__, "bl", self.bl)
+                await hikka_utils.answer(m, f"{uid} из чс")
+        except: pass
+
+    async def флудcmd(self, m):
+        if not await self.check_sub(): return
+        args = hikka_utils.get_args_raw(m).split()
+        cnt = 150
+        r = await m.get_reply_message()
+        if r:
+            ent = r.chat_id
+            if args and args[0].isdigit(): cnt = int(args[0])
+        else:
+            if not args: return
+            ent = await self.client.get_entity(args[0])
+            if len(args)>1 and args[1].isdigit(): cnt = int(args[1])
+        if self.flood: self.flood.cancel()
+        self.flood = asyncio.create_task(self.do_flood(ent, cnt))
+        await hikka_utils.answer(m, "флуд по полной")
+
+    async def стопфлудcmd(self, m):
+        if self.flood:
+            self.flood.cancel()
+            self.flood = None
+            await hikka_utils.answer(m, "стоп флуд")
+
+    async def do_flood(self, ent, cnt):
+        i=0
+        while i<cnt:
+            try:
+                await self.client.send_message(ent, "🔥🔥🔥")
+                i+=1
+                await asyncio.sleep(0.5)
+            except: break
+
+    async def antistealcmd(self, m):
+        if not await self.check_sub(): return
+        args = hikka_utils.get_args_raw(m)
+        if args=="on":
+            self.antisteal=True
+            self.db.set(__name__, "antisteal", True)
+            await hikka_utils.answer(m, "антистиллер вкл")
+        elif args=="off":
+            self.antisteal=False
+            self.db.set(__name__, "antisteal", False)
+            await hikka_utils.answer(m, "антистиллер выкл")
+        else:
+            await hikka_utils.answer(m, f"статус: {'вкл' if self.antisteal else 'выкл'}")
+
+    async def threataddcmd(self, m):
+        if not await self.check_sub(): return
+        args = hikka_utils.get_args_raw(m)
+        if args and args.lower() not in self.threats:
+            self.threats.append(args.lower())
+            self.db.set(__name__, "threats", self.threats)
+            await hikka_utils.answer(m, f"добавил угрозу {args}")
+
+    async def кmcmd(self, m):
+        await hikka_utils.answer(m, self.strings["km_help"])
+
+@loader.author("@tu2de (laironix)")
