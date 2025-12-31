e79292495e8266e57bd000d2981db3e67fc33a04
System934 <p98851642@gmail.com>
2025-12-31 17:38:05 +0800

    Update and rename load_full.lua to Thelostfront.lua
    Signed-off-by: System934 <p98851642@gmail.com>

---
+++ Thelostfront.lua
@@ -0,0 +1,9 @@
+
+
+-- ===== URL =====
+local SCRIPT_URL = "loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/5ec621db2a68f482"))()"
+
+-- ===== MAIN LOADSTRING =====
+loadstring(
+	game:HttpGet(SCRIPT_URL)
+)()
--- load_full.lua
+++
@@ -1,9 +0,0 @@
-
-
--- ===== URL =====
-local SCRIPT_URL = "https://api.junkie-development.de/api/v1/luascripts/public/8381f8030a22565ce93fbcbb4c15a653693b0076ded98d2575456f65e5714e08/download"
-
--- ===== MAIN LOADSTRING =====
-loadstring(
-	game:HttpGet(SCRIPT_URL)
-)()
